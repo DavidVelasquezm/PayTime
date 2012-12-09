2c80670346c000e394dec1f91a4b98b953b71b39
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2012-12-09 13:47:47 -0500

    Agregados los archivos base

---
+++ f_log.php
@@ -0,0 +1,19 @@
+     				<div class="cnt_b">
+					<div class="formRow" align="center">
+						<div class="input-prepend">
+							<span class="add-on"><i class="icon-user"></i></span><input type="text" id="username" name="username" placeholder="Username" />
+						</div>
+					</div>
+					<div class="formRow" align"center">
+						<div class="input-prepend">
+							<span class="add-on"><i class="icon-lock"></i></span><input type="password" id="password" name="password" placeholder="Password" />
+						</div>
+					</div>
+<br>
+				</div>
+				<div class="btm_b clearfix">
+					<button class="btn btn-inverse pull-center" type="submit" id="btsub" name="login" value="LogIn">Sign In</button>
+					| <span class="link_reg"><a href="#register">Register</a></span> |
+					<span class="link_reg"><a href="#pass_form">Forgot password?</a></span>
+				</div>  
+
