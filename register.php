3369c6610729f583e59933c6d529356049d78c31
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2012-12-09 23:29:55 -0500

    agregado el estilo al registro

--- register.php
+++ register.php
@@ -10,11 +10,49 @@
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-    <link href="bootstrap/css/custom.css" rel="stylesheet" media="screen">
+    
     <!-- Bootstrap -->
+
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
   </head>
   <body>
+
     <div id="login_div" name="login_div">
+      <div class="container">
+    <div class="form-signin"> 
+      <h2 class="form-signin-heading">Registrate</h2>
       <div class="control-group">
         <label class="control-label" for="inputEmail">Nombre</label>
         <div class="controls">
@@ -33,10 +71,13 @@
           <input type="password" placeholder="Password"  id="password" name="password">
         </div>
       </div>
+
       <div class="control-group">
         <div class="controls">
-          <button type="submit" id="regbtn" name="regbtn" class="btn">Register</button>
+          <button type="submit" id="regbtn" name="regbtn" class="btn btn-large btn-primary">Register</button>
         </div>
+      </div>
+      </div>
       </div>
     </div>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
