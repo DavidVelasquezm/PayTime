e77e56f7deeca9bc4bd72c8c29d7ebe3aafe5886
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2012-12-14 11:49:00 -0500

    agregado menu de registro

--- f_log.php
+++ f_log.php
@@ -1,3 +1,24 @@
+     				<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">PayTime</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="./">inicio</a></li>
+              <li><a href="register.php">Registrate</a></li>
+              <li><a href="#about">Información</a></li>
+              <li><a href="#contact">Contactenos</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
      				<div class="cnt_b" id="loginfix">
 					<div class="formRow" align="center">
 						<div class="input-prepend">
