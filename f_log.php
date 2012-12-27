2874d4bcce19a1d2b8755d613a91e6819f57aaf4
Cristyan Sepúlveda V <lordbrayam@gmail.com>
2012-12-27 15:01:17 -0500

    eliminada la doble barra

--- f_log.php
+++ f_log.php
@@ -1,42 +1,21 @@
-﻿     				<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#">PayTime</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="./">inicio</a></li>
-              <li><a href="register.php">Registrate</a></li>
-              <li><a href="#forgot">Recuperar contraseña</a></li>
-              <li><a href="#contact">Contactenos</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-
-     				<div class="cnt_b" id="loginfix">
-					<div class="formRow" align="center">
-						<div class="input-prepend">
-							<span class="add-on"><i class="icon-user"></i></span><input type="text" id="username" name="username" placeholder="Email" />
-						</div>
-					</div>
-					<div class="formRow" align"center">
-						<div class="input-prepend">
-							<span class="add-on"><i class="icon-lock"></i></span><input type="password" id="password" name="password" placeholder="Password" />
-						</div>
-					</div>
+﻿            <div class="cnt_b" id="loginfix">
+          <div class="formRow" align="center">
+            <div class="input-prepend">
+              <span class="add-on"><i class="icon-user"></i></span><input type="text" id="username" name="username" placeholder="Email" />
+            </div>
+          </div>
+          <div class="formRow" align"center">
+            <div class="input-prepend">
+              <span class="add-on"><i class="icon-lock"></i></span><input type="password" id="password" name="password" placeholder="Password" />
+            </div>
+          </div>
 <br>
-				</div>
-				<div class="btm_b clearfix">
-					<button class="btn btn-inverse pull-center" type="submit" id="btsub" name="login" value="LogIn">Sign In</button>
-					| <span class="link_reg"><a href="register.php">Register</a></span> |
-					<span class="link_reg"><a href="reset.php">Forgot password?</a></span>
-				</div>  
+        </div>
+        <div class="btm_b clearfix">
+          <button class="btn btn-inverse pull-center" type="submit" id="btsub" name="login" value="LogIn">Sign In</button>
+          | <span class="link_reg"><a href="register.php">Register</a></span> |
+          <span class="link_reg"><a href="reset.php">Forgot password?</a></span>
+        </div>  
 </div>
 <script type="text/javascript">
 
